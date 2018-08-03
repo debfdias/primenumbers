b5714f0f170d8e289bc1d95a486215c3dc652f6e
debfdias <dfd2@cin.ufpe.br>
2018-08-02 22:47:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ io.configure(function () {
 
 
 // Use the port that Heroku provides or default to 5000
-var port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+var port = process.env.PORT || 5000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.listen(port, function() {
